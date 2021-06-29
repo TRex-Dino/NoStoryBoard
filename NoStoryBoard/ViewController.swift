67e570ba3f3a2deff07790c1e1d82e0b0537e4aa
Dima <dmitrysinkov6@gmail.com>
2021-06-29 15:04:04 +0300

    Initial Commit

---
+++ NoStoryBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryBoard
+//
+//  Created by Dmitry on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
